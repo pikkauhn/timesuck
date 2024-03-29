aa912157a4cc51bf07156f80ab2ab69c26896264
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2024-03-29 12:10:25 -0500

    Update SigninButton.tsx

--- app/components/system/SigninButton.tsx
+++ app/components/system/SigninButton.tsx
@@ -1,10 +1,38 @@
 'use client'
-import React from 'react';
+import React, { useEffect } from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { Button } from 'primereact/button';
+import showMessage from './ShowMessage';
+import { SupabaseClient } from './SupaConx';
+import { MessageSeverity } from 'primereact/api';
+
+const supabase = SupabaseClient.getSupabaseClient();
 
 const SigninButton = () => {
     const { data: session } = useSession();    
+
+    useEffect(() => {
+        let ignore = false
+        const loadData = async () => {
+          try {
+            // Notify the user that the server is spinning up
+            showMessage('warn', 'Server Spinning Up', 'Please wait...');
+            const { data:result, error } = await supabase
+            .from("users")
+            .select('firstName')
+            
+            // Update the message once the server is up
+            showMessage('success', 'Server is Up', 'Data loaded successfully.');
+          } catch (error) {
+            console.log(error);
+            // Notify the user about the server error
+            showMessage('error', 'Server Error', 'An error occurred.');
+          }
+        };
+    
+        loadData();
+      }, []);
+
     if (session && session.user) {
 
         return (
